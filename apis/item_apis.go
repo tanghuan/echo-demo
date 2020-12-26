b0a2124a37c9804bcce084c80dbc342925edde1f
tanghuan <tanghuanyou@163.com>
2020-12-26 20:14:39 +0800

    add aliyun oss client

---
+++ apis/item_apis.go
@@ -0,0 +1,21 @@
+package apis
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/aliyun/aliyun-oss-go-sdk/oss"
+)
+
+func init() {
+	fmt.Println("package apis init aliyun oss client ......")
+	endpoint := "http://dev.oss-cn-chengdu.aliyuncs.com"
+	accessKeyID := "asdfasdf"
+	accessKeySecret := "asdfasdf"
+	client, err := oss.New(endpoint, accessKeyID, accessKeySecret, oss.UseCname(true))
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(-1)
+	}
+	fmt.Println(client)
+}
