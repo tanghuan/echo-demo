eeef133dcb42e8fd04e7241b597692e54426ab94
tanghuan <tanghuanyou@163.com>
2021-01-05 17:46:35 +0800

    update code and add .gitignore

--- config/config.go
+++ config/config.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 
 	"github.com/dgrijalva/jwt-go"
 )
@@ -20,6 +21,21 @@ var (
 
 	// SignKey rsa jwt 签发私钥
 	SignKey *rsa.PrivateKey
+
+	// DatabaseHost 数据库地址
+	DatabaseHost = os.Getenv("DatabaseHost")
+
+	// DatabasePort 数据库端口
+	DatabasePort = os.Getenv("DatabasePort")
+
+	// DatabaseName 数据库名称
+	DatabaseName = os.Getenv("DatabaseName")
+
+	// DatabaseUsername 数据库链接用户名
+	DatabaseUsername = os.Getenv("DatabaseUsername")
+
+	// DatabasePassword 数据库链接密码
+	DatabasePassword = os.Getenv("DatabasePassword")
 )
 
 func init() {
@@ -44,4 +60,29 @@ func init() {
 		log.Fatal(err)
 	}
 
+	if DatabaseHost == "" {
+		DatabaseHost = "127.0.0.1"
+	}
+
+	if DatabasePort == "" {
+		DatabasePort = "3306"
+	}
+
+	if DatabaseName == "" {
+		DatabaseName = "echo-demo"
+	}
+
+	if DatabaseUsername == "" {
+		DatabaseUsername = "root"
+	}
+
+	if DatabasePassword == "" {
+		DatabasePassword = "admin"
+	}
+
+}
+
+// GetDSN 获取数据库链接
+func GetDSN() string {
+	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", DatabaseUsername, DatabasePassword, DatabaseHost, DatabasePort, DatabaseName)
 }
--- persist/persist.go
+++ persist/persist.go
@@ -4,6 +4,7 @@ import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 
+	"skinshub-api/config"
 	"skinshub-api/persist/entities"
 )
 
@@ -12,9 +13,9 @@ var Con *gorm.DB
 
 // 初始化数据库链接
 func init() {
-	dsn := "root:admin@tcp(127.0.0.1:3306)/echo-demo?charset=utf8mb4&parseTime=True&loc=Local"
+	// dsn := "root:admin@tcp(127.0.0.1:3306)/echo-demo?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.New(mysql.Config{
-		DSN:                      dsn,
+		DSN:                      config.GetDSN(),
 		DisableDatetimePrecision: true,
 	}), &gorm.Config{})
 	if err != nil {
